6fd6517128438a71bfbff1bf65fe49d29e2b3379
ColdIV <yufoolme@gmail.com>
2020-03-27 20:36:34 +0100

    added Point class

---
+++ Point.cpp
@@ -0,0 +1,30 @@
+#include "Point.h"
+
+Point::Point () {
+	this->setPoint(0, 0);
+}
+
+Point::Point (float x, float y) {
+	this->setPoint(x, y);
+}
+
+float Point::getX () {
+	return this->x;
+}
+
+float Point::getY () {
+	return this->y;
+}
+
+void Point::setPoint (float x, float y) {
+	this->setX(x);
+	this->setY(y);
+}
+
+void Point::setX (float x) {
+	this->x = x;
+}
+
+void Point::setY (float y) {
+	this->y = y;
+}
