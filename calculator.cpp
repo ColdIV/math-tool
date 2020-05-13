3a6042fcc21250848f7b609896a2eedca2284c02
laser-shark <amazing.laser.sharks@gmail.com>
2020-05-13 21:35:39 +0200

    Standardize callcing of calculator functionality

---
+++ calculator.cpp
@@ -0,0 +1,39 @@
+#include "calculator.h"
+
+void calculate(istream* s) {
+	// TokenStream takes an "istream" pointer and splits it into specific Tokens.
+	// Tokens are a specific "kind" which defines what the Parser (calculator)
+	// will do with them.
+	TokenStream TS(s);
+	
+	// The Parser handles the main calculation. It is initialized with a pointer 
+	// to the TokenStream object. It uses recursive descent to handle complex
+	// expressions.
+	Parser P(&TS);
+	
+	while(true) {
+		// get the first Token from the TokenStream
+		TS.get();
+		if (TS.current().kind == 'p') continue; // print the result of a calculation after a ";" or "\n"
+		if(TS.current().kind == 'e') break; // end the calculation after "q", "e" or ctrl+d (input stream EOF)
+		
+		double res = P.expr(false); // main Parser entrypoint. Calling expr with false means that it will
+									   // not get a new Token from the TokenStream.
+		
+		if(P.getError()) {
+			std::cout << P.getErrorMsg() << endl;
+		} else {
+			std::cout << res << endl;
+		}
+	}
+	
+	P.listMemory();
+}
+
+void calculate(string s) {
+	// string has to be converted to stream before passing it to TokenStream
+	istringstream stream1;
+	stream1.str(s);
+	
+	calculate(&stream1);
+}
---
+++ calculator.h
@@ -0,0 +1,14 @@
+#ifndef _CALCULATOR_H_
+#define _CALCULATOR_H_
+
+#include <iostream>
+#include <string>
+#include <sstream>
+
+#include "TokenStream.h"
+#include "Parser.h"
+
+void calculate(istream* s);
+void calculate(string s);
+
+#endif
