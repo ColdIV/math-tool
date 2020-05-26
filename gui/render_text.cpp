3ce9a838c2a1d741a80d2f5fe71e3f0a819e794a
laser-shark <amazing.laser.sharks@gmail.com>
2020-05-26 23:03:57 +0200

    Calculate line spacing dynamically

--- gui/render_text.cpp
+++ gui/render_text.cpp
@@ -17,10 +17,11 @@ void render_text(
 	// helper variables
 	int textW = xEnd - xStart;
 	int textH = yEnd - yStart;
+	int lineHeight = fontSize * 1.4; // determine space between lines
 
 	// starting point of message rectangle
 	int startX = xStart;
-	int startY = yStart - 70;
+	int startY = yStart - lineHeight;
 
 	// text has to be converted to istream in order to call getline
 	std::stringstream ss(text);
@@ -28,7 +29,7 @@ void render_text(
 
 	// break at newline and render single line
 	while(getline(ss, textToRender, '\n')) {
-		startY += 70;
+		startY += lineHeight;
 		SDL_Surface *textSurface = TTF_RenderText_Solid(
 			font, textToRender.c_str(), color
 		);
