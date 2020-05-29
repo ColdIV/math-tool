98511eae4d47b9241fdd0fb2fccb92879d3fd391
ColdIV <yufoolme@gmail.com>
2020-05-30 00:27:46 +0200

    added pointToString method to object added method to make object virtual

--- backend/Object.h
+++ backend/Object.h
@@ -2,6 +2,9 @@
 #define _OBJECT_H_
 
 #include <vector>
+#include <string>
+#include <iomanip>
+#include <sstream>
 #include "Point.h"
 
 class Object {
@@ -13,6 +16,8 @@ class Object {
 		Object ();
 		
 		Object (Point point);
+
+		virtual void makeVirtual () {}
 		
 		void setPoint (int n, Point point);
 
@@ -23,6 +28,8 @@ class Object {
 		Point getPoint (int n);
 		
 		std::vector <Point> getPoints ();
+
+		std::string pointsToString ();
 };
 
 #endif
