7cf4cb1c962081d55a9114a8aeb3d023f157bd2d
ColdIV <yufoolme@gmail.com>
2020-05-30 00:29:45 +0200

    added GeometryParser class (not fully functional yet)

---
+++ backend/GeometryParser.cpp
@@ -0,0 +1,115 @@
+#include "GeometryParser.h"
+#include <iostream>
+
+GeometryParser::GeometryParser () {
+    this->objectNames = std::vector <std::string> (6);
+    this->objectNames = {"point", "circle", "line", "triangle", "square", "rectangle"};
+
+    this->functionNames = std::vector <std::string> (2);
+    this->functionNames = {"angle", "intersection"};
+}
+
+int GeometryParser::identify (std::string s) {
+    // check for object
+    for (int i = 0; i < this->objectNames.size(); ++i) {
+        std::size_t found = s.find(this->objectNames[i]);
+
+        if (found != std::string::npos) {
+            return 1;
+        }
+    }
+
+    // check for function
+    for (int i = 0; i < this->functionNames.size(); ++i) {
+        std::size_t found = s.find(this->functionNames[i]);
+
+        if (found != std::string::npos) {
+            return 0;
+        }
+    }
+
+    return -1;
+}
+
+std::unordered_map <std::string, Object*> GeometryParser::parseToObject (std::string s) {
+    std::string objName = "";
+    int objParams = 0;
+
+    // find the object
+    for (int i = 0; i < this->objectNames.size(); ++i) {
+        std::size_t found = s.find(this->objectNames[i]);
+
+        if (found != std::string::npos) {
+            objName = this->objectNames[i];
+            objParams = (int)found + objName.length();
+            break;
+        }
+    }
+
+    // if objParams > 0, we found an object to work with
+    if (objParams) {
+        // find all numbers
+        int sign = 1;
+        std::vector <double> numbers (0);
+        
+        for (int i = objParams; i < s.length(); ++i) {
+            if (isdigit(s[i])) {
+                // found number
+                if (s[i] - 1 == '-') {
+                    sign = -1;
+                }
+
+                double tmpNumber = std::stod(s.substr(i));
+                tmpNumber *= sign;
+                numbers.push_back(tmpNumber);
+
+                size_t cPos = s.substr(i).find(",");
+                size_t pPos = s.substr(i).find("|");
+                size_t pos;
+
+                if (cPos == std::string::npos && pPos == std::string::npos) break;
+                if (cPos == std::string::npos) pos = pPos;
+                else if (pPos == std::string::npos) pos = cPos;
+                else pos = std::min(cPos, pPos);
+
+                i += pos;
+            }
+        }
+
+        // create object and add to map
+        if (objName == "point") {
+            if (numbers.size() == 2) {
+                Circle *tmp = new Circle(Point (numbers[0], numbers[1]), 1);
+                this->objects["P" + std::to_string(++this->objectNum['P'])] = tmp;
+            }
+        } else if (objName == "circle") {
+            if (numbers.size() == 3) {
+                Circle *tmp = new Circle(Point (numbers[0], numbers[1]), numbers[3]);
+                this->objects["C" + std::to_string(++this->objectNum['C'])] = tmp;
+            }
+        } else if (objName == "line") {
+            if (numbers.size() == 4) {
+                Line *tmp = new Line(Point (numbers[0], numbers[1]), Point (numbers[2], numbers[3]));
+                this->objects["L" + std::to_string(++this->objectNum['L'])] = tmp;
+            }
+        } else if (objName == "triangle") {
+            if (numbers.size() == 6) {
+                Triangle *tmp = new Triangle(Point (numbers[0], numbers[1]), Point (numbers[2], numbers[3]), Point (numbers[4], numbers[5]));
+                this->objects["T" + std::to_string(++this->objectNum['T'])] = tmp;
+            }
+        } else if (objName == "square") {
+            if (numbers.size() == 4) {
+                Square *tmp = new Square(Point (numbers[0], numbers[1]), Point (numbers[2], numbers[3]));
+                this->objects["S" + std::to_string(++this->objectNum['S'])] = tmp;
+            }
+        } else if (objName == "rectangle") {
+            if (numbers.size() == 8) {
+                Rectangle *tmp = new Rectangle(Point (numbers[0], numbers[1]), Point (numbers[2], numbers[3]), Point (numbers[4], numbers[5]), Point (numbers[6], numbers[7]));
+                this->objects["R" + std::to_string(++this->objectNum['R'])] = tmp;
+            }
+        }
+    }
+
+    // return map of objects
+    return this->objects;
+}
---
+++ backend/GeometryParser.h
@@ -0,0 +1,32 @@
+#ifndef _GEOMETRYPARSER_H_
+#define _GEOMETRYPARSER_H_
+
+#include "Object.h"
+#include "Point.h"
+#include "Circle.h"
+#include "Line.h"
+#include "Triangle.h"
+#include "Square.h"
+#include "Rectangle.h"
+#include <unordered_map>
+#include <string>
+
+class GeometryParser {
+    private:
+        std::unordered_map <std::string, Object*> objects;
+        std::unordered_map <char, int> objectNum;
+
+        std::vector <std::string> objectNames;
+        std::vector <std::string> functionNames;
+
+    public:
+        GeometryParser ();
+
+        // returns 1 for object, 0 for function, -1 for invalid string
+        int identify (std::string s);
+
+        std::unordered_map <std::string, Object*> parseToObject (std::string s);
+
+};
+
+#endif
