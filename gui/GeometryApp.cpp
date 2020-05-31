3bab7c5fb62afe2e3b2042e26710c835b6cac5c3
laser-shark <amazing.laser.sharks@gmail.com>
2020-05-31 19:55:43 +0200

    Display the created objects in Geometry App

--- gui/GeometryApp.cpp
+++ gui/GeometryApp.cpp
@@ -5,6 +5,11 @@ GeometryApp::GeometryApp(SDL_Window *win, SDL_Renderer *ren) : App(win, ren) {
 	// widget for coordinate system and objects
 	Graph *graph = new Graph(win, ren, 30, 30, 600, 400, "objects");
 
+	// widget for displaying the created objects
+	TextOutput *createdObjects = new TextOutput{
+		win, ren, 0, 500, 700, 600, "Angelegte Objekte: ", 18
+	};
+
 	// widget for input prompt
 	TextOutput *prompt = new TextOutput{
 		win, ren, 0, 450, 600, 500, "Eingabe ('q' zum Beenden): ", 24
@@ -13,10 +18,12 @@ GeometryApp::GeometryApp(SDL_Window *win, SDL_Renderer *ren) : App(win, ren) {
 	// widget for input
 	this->parser = new GeometryParser{};
 	GeometryInput *input = new GeometryInput{
-		win, ren, 320, 450, 600, 500, "", 24, graph, this->parser
+		win, ren, 320, 450, 600, 500, "", 24, graph, this->parser,
+		createdObjects
 	};
 
 	this->widgets.push_back(graph);
+	this->widgets.push_back(createdObjects);
 	this->widgets.push_back(prompt);
 	this->widgets.push_back(input);
 }
