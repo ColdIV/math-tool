2a427486ffc9a4e178d5b1b310d591e52b395110
laser-shark <amazing.laser.sharks@gmail.com>
2020-06-01 21:40:26 +0200

    Change comment

--- gui/App.cpp
+++ gui/App.cpp
@@ -17,7 +17,7 @@ App* App::mainloop() {
 	while(true) {
 		while(SDL_PollEvent(&event)) {
 			if(event.type == SDL_QUIT) {
-				return nullptr; // nullptr means we want to stay in the running app
+				return nullptr; // nullptr means we want to end the program
 			}
 
 			for (Widget *widget : this->widgets) {
